23a4515321e80365d2d5ebd3d2be46ea4d208cd7
irakli nozadze <iraklinozadze2509@gmail.com>
2023-03-01 14:20:01 +0400

    Initial Commit

---
+++ FindYourMusic+/FindYourMusic_App.swift
@@ -0,0 +1,17 @@
+//
+//  FindYourMusic_App.swift
+//  FindYourMusic+
+//
+//  Created by Irakli Nozadze on 01.03.23.
+//
+
+import SwiftUI
+
+@main
+struct FindYourMusic_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
